396a3adbb7ddce6b6dcf773a36eb2e84fbfec3ce
msitabheer20 <msitabheerdey@gmail.com>
2023-12-24 14:44:19 +0530

    first commit

---
+++ app/(marketing)/_components/index.ts
@@ -0,0 +1,7 @@
+import { Heading } from "./heading";
+import { Heroes } from "./heroes";
+import { Footer } from "./footer";
+import { Logo } from './logo'
+import { Navbar } from "./navbar";
+
+export {Heading, Heroes, Footer, Logo, Navbar}
